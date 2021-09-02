627ad739c2ab8b0ea7272a66ba93954c62e75d23
Sergio de Souza Silva <79226135+SergSouza@users.noreply.github.com>
2021-09-02 09:15:56 -0300

    Revert "alterando nossa splash e criando nossa tela de login"

--- index.py
+++ index.py
@@ -1,16 +1,12 @@
 from tkinter import ttk
 from tkinter import *
 from intro import splash
-from usuario import Login
 
-class Main_Principal(splash,Login):
+class Main_Principal(splash):
     def __init__(self):
         global b 
         if b == 0:
             super().__init__()
-            b==1
-        Login.__init__(self)
-        self.loginW.mainloop()
 
 if __name__ == '__main__':
     b = 0 
