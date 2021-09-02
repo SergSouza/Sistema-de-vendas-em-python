e631273f876d028a8cdaa080f15babab488d4ab3
Sergio Souza <sergio.escureto@gmail.com>
2021-09-02 09:36:52 -0300

    alterando nossa palhetas de cores (backgrounds)

--- intro.py
+++ intro.py
@@ -10,7 +10,7 @@ class splash:
         Width = 1000
         height = 680
 
-        self.splashMainW.config(bg='green')
+        self.splashMainW.config(bg='#BF8C4E')
         tela_largura = self.splashMainW.winfo_screenwidth()
         tela_altura = self.splashMainW.winfo_screenheight()
 
@@ -30,22 +30,22 @@ class splash:
 
 
         #trabalhando nosso label
-        main = LabelFrame(self.splashMainW,width=890,height=560,bg='darkgreen', relief='sunken')
+        main = LabelFrame(self.splashMainW,width=890,height=560,bg='#8C8177', relief='sunken')
         main.place(x=55,y=70)
 
         #trabalhando nossa imagen
-        FotoFrame = LabelFrame(main,width=420,height=444,bg='darkgreen', relief='raised')
+        FotoFrame = LabelFrame(main,width=420,height=444,bg='#8C8177',bd=0, relief='raised')
         foto =  Label(FotoFrame,image=img)
         foto.place (x=6,y=6)
         FotoFrame.place(x=10,y=100)
 
         #Label(main, text="", font="",bg="").place(x="",y="")
-        Label(main,text="Sistema de vendas em python sqlite",font="arial 28 bold",bg="darkgreen").place(x="45",y="10")               
-        Label(main,text="Faculdade FATEPI",font="arial 32 bold",bg="darkgreen").place(x="450",y="160")
-        Label(main,text="CURSO: python (Prof= Aratã)",font="arial 16 bold",bg="darkgreen").place(x="445",y="260")
-        Label(main,text="DESENVOLVEDOR: Sergio Souza",font="arial 16 bold",bg="darkgreen").place(x="445",y="310")
-        Label(main,text="EMAIL: sergio.escureto@gmail.com",font="arial 16 bold",bg="darkgreen").place(x="445",y="360")
-        Label(main,text="TELEFONE: (99) 984717748",font="arial 16 bold",bg="darkgreen").place(x="445",y="410")
+        Label(main,text="Sistema de vendas em python sqlite",font="arial 28 bold",bg="#8C8177").place(x="45",y="10")               
+        Label(main,text="Faculdade FATEPI",font="arial 32 bold",bg="#8C8177").place(x="450",y="160")
+        Label(main,text="CURSO: python (Prof= Aratã)",font="arial 16 bold",bg="#8C8177").place(x="445",y="260")
+        Label(main,text="DESENVOLVEDOR: Sergio Souza",font="arial 16 bold",bg="#8C8177").place(x="445",y="310")
+        Label(main,text="EMAIL: sergio.escureto@gmail.com",font="arial 16 bold",bg="#8C8177").place(x="445",y="360")
+        Label(main,text="TELEFONE: (99) 984717748",font="arial 16 bold",bg="#8C8177").place(x="445",y="410")
 
         self.Continuar = Button(self.splashMainW,width=20,text="Continuar",anchor="center",bg="green",fg="dimgray",font="roboto 14",foreground="white")
         self.Continuar.place(x=600,y=580)
--- usuario.py
+++ usuario.py
@@ -17,7 +17,7 @@ class Login :
 
 
         #configurando nossa tela e posicionamento 
-        self.loginW.config(bg="darkblue")
+        self.loginW.config(bg="#BF8C4E")
         tela_largura = self.loginW.winfo_screenwidth()
         tela_altura = self.loginW.winfo_screenheight()
         x = (tela_largura / 2 ) - (width / 2 )
@@ -34,10 +34,10 @@ class Login :
     #criando nossos campo de interaçao na nossa tela de login
     def objetos_login(self):
         #criando uma campo onde vamos colocar nossos imputs
-        self.longiFrame = LabelFrame(self.loginW,bg="#4267b2",height=400,width=300)
+        self.longiFrame = LabelFrame(self.loginW,bg="#BFBBB4",height=400,width=300)
         self.longiFrame.place(x=103,y=95)
         # colocando nosso title
-        self.toplabel = Label(self.longiFrame,fg="white", bg="#4267b2", anchor="center",text="Login",font="arial 35 bold")
+        self.toplabel = Label(self.longiFrame,fg="black", bg="#BFBBB4", anchor="center",text="Login",font="arial 35 bold")
         self.toplabel.place(x=75,y=25)
         #nossos campos
         self.us = ttk.Entry(self.longiFrame, textvariable=self.username, width=20,font="Roboto 14")
@@ -51,7 +51,7 @@ class Login :
         self.pa.bind('<Button-1>',self.onclick1)
 
         #criando nosso botão de entrar
-        self.signin = Button(self.longiFrame,width=10,text="Login",anchor="center",bg="darkgreen",fg="dimgray",font="roboto 14",foreground="white")
+        self.signin = Button(self.longiFrame,width=10,text="Login",anchor="center",bg="#30B8B2",fg="dimgray",font="roboto 14",foreground="white")
         self.signin.place(x=35,y=290)
 
 
